711c0ed5cad619d5b62448e290f8d34e957a540e
Bruno Bonfils <asyd@asyd.net>
2022-11-23 23:41:53 +0100

    First release

--- console.py
+++ console.py
@@ -1,155 +1,167 @@
 #!/usr/bin/env python
 
-from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.reactive import reactive
-from textual.widgets import Header, Footer, DataTable
-from dataclasses import dataclass, field
-from rich.tree import Tree
-
 import argparse
-import certifi
 import logging
 import os
-import requests
+from dataclasses import dataclass, field
+from functools import lru_cache
+from typing import Type
 
+import certifi
+import requests
+from rich.rule import Rule
+from rich.text import Text
+from rich.tree import Tree
+from textual import events
+from textual.app import App, ComposeResult, CSSPathType
+from textual.containers import Container, Horizontal, Vertical
+from textual.driver import Driver
+from textual.reactive import reactive
+from textual.screen import Screen
+from textual.widgets import Checkbox, DataTable, Footer, Header, Label, Static
+
+from nomad import NomadCluster
 
 logger = logging.getLogger()
 
 
-@dataclass
-class NomadCluster:
-  url: str
-  namespace: str | None
-  client_cert: str | None
-  client_key: str | None
-  ca: str | None
-  token: str | None
-
-  @classmethod
-  def from_environ(cls):
-    return cls(
-      # Remove final slash if exists
-      url = os.environ['NOMAD_ADDR'].rstrip('/'),
-      namespace = os.getenv('NOMAD_NAMESPACE', None),
-      client_cert = os.getenv('NOMAD_CLIENT_CERT', None),
-      client_key = os.getenv('NOMAD_CLIENT_CERT', None),
-      ca = os.getenv('NOMAD_CACERT', None),
-      token= os.getenv('NOMAD_TOKEN', None)
-    )
-
-@dataclass
-class NomadJob:
-  name: str
-  status: str
-  type: str
-  deployment: str = "unknown"
-  subtasks: list[str] = field(default_factory=list)
-  tasks_running: int = 0
-
-
-def nomad_request(cluster: NomadCluster, url: str, method: str ="get", **kwargs) -> requests.Response:
-  client_cert = (cluster.client_cert, cluster.client_key)
-  truststore = certifi.where()
-  if cluster.ca:
-    truststore = cluster.ca
-
-  r = requests.request(
-    method,
-    url, 
-    cert=client_cert,
-    verify=truststore,
-    headers={'X-Nomad-Token': cluster.token} )
-
-  logger.debug("Performed %s %s, got: %s", method, url, r.status_code)
-
-  return r
-
-def nomad_get_jobs(cluster: NomadCluster) -> list[NomadJob]:
-  job_url = f"{cluster.url}/v1/jobs?"
-  deployments_url = f"{cluster.url}/v1/deployments?"
-  if cluster.namespace:
-    job_url += f"namespace={cluster.namespace}&"
-    deployments_url += f"namespace={cluster.namespace}&"
-
-  jobs = []
-  r = nomad_request(cluster, job_url)
-  deployments = {k['JobID']: k for k in nomad_request(cluster, deployments_url).json()}
-
-  for job in r.json():
-    job_name = job['Name']
-    try:
-      last_deployment = deployments[job_name]['Status']
-    except KeyError:
-      last_deployment = 'unknown'
-    jobs.append(
-      NomadJob(
-        name=job_name, 
-        status=job['Status'], 
-        type=job['Type'],
-        deployment=last_deployment,
-        subtasks=job['JobSummary']['Summary'].keys(),
-        ))
-  return jobs
-
-
 class NomadJobsWidget(DataTable):
 
-  jobs = reactive(list[NomadJob])
+    def on_mount(self) -> None:
+        self.add_column("Name")
+        self.add_column("Status")
+        self.add_column("Type")
+        self.add_column("Last deployment")
+        self.add_column("Subtasks")
+        self.update_jobs()
+        self.set_interval(1, self.update_jobs)
 
-  def on_mount(self) -> None:
-    self.cluster = NomadCluster.from_environ()
-    self.update_jobs()
-    self.add_column('Name')
-    self.add_column('Status')
-    self.add_column('Type')
-    self.add_column('Last deployment')
-    self.add_column('Subtasks')
-    self.set_interval(1, self.update_jobs)
+    def update_jobs(self) -> None:
+        self.clear()
+        self.app.cluster.refresh_jobs()
+        for name, job in self.app.cluster.jobs.items():
+            tasks_height = len(job.tasks)
+            tasks = Tree("Tasks", hide_root=True)
+            for task_name, task in job.tasks.items():
+                if job.type == 'system':
+                    tasks.add(Text(f"{task_name} ({task.running})"))
+                else:
+                    style = "green"
+                    if task.running != task.expected:
+                        style = "red"
+                    tasks.add(Text(f"{task_name} ({task.running} / {task.expected})", style=style))
+            
+            if job.deployment == "successful":
+                deployment = Text(job.deployment, style="green")
+            elif job.deployment == "failed":
+                deployment = Text(job.deployment, style="red")
+            else:
+                deployment = Text(job.deployment, style="dark_orange3")
 
-  def update_jobs(self) -> None:
-    self.clear()
-    for job in nomad_get_jobs(self.cluster):
-      self.add_row(
-        job.name,
-        job.status,
-        job.type,
-        job.deployment,
-        ", ".join(job.subtasks),
-      )
+            self.add_row(
+                name,
+                job.status,
+                job.type,
+                deployment,
+                tasks,
+                height=tasks_height,
+            )
+
+        # for job in nomad_get_jobs(self.app.cluster, job_type="service"):
+        #     tasks_height = len(job.tasks)
+        #     tasks = Tree("Tasks", hide_root=True)
+        #     for task in job.tasks:
+        #         tasks.add(f"{task.name} ({task.running} / {task.expected})")
+
+        #     if job.deployment == "successful":
+        #         deployment = Text("successful", style="green")
+        #     elif job.deployment == "failed":
+        #         deployment = Text("failed", style="red")
+        #     else:
+        #         deployment = Text(job.deployment, style="#FF7F50")
+        #     self.add_row(
+        #         job.name,
+        #         job.status,
+        #         job.type,
+        #         deployment,
+        #         tasks,
+        #         height=tasks_height,
+        #     )
 
 
-class NomadMonitor(App):
-  BINDINGS = [("q", "quit", "Quit")]
+class Filter(Screen):
+    """Display filters"""
 
-  def compose(self) -> ComposeResult:
-    yield Header()
-    yield Container(NomadJobsWidget(id='jobs'), DataTable(id='tasks'))
-    yield Footer()
+    DEFAULT_CSS = """
+    Screen {
+        margin: 4 8;
+        layout: grid;
+        content-align: center middle;
+        grid-size: 2 2;
+    }
+    """
+
+    BINDINGS = [("escape", "app.pop_screen", "close")]
+
+    def __init__(
+        self, name: str | None = None, id: str | None = None, classes: str | None = None
+    ) -> None:
+        super().__init__(id="filter")
+
+    def compose(self) -> ComposeResult:
+        yield Static("running")
+        yield Checkbox(id="job_running")
+        yield Static("dead")
+        yield Checkbox(id="job_dead")
+        yield Footer()
+
+
+class NomadMonitor(Screen):
+    BINDINGS = [("f", "app.push_screen('filter')", "Filters")]
+
+    def __init__(self):
+        super().__init__()
+
+    # Force focus on job list
+    def on_mount(self) -> None:
+        self.set_focus(self.query_one("#jobs"))
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Container(
+            NomadJobsWidget(id="jobs"),
+        )
+        # yield Container(id="status")
+        yield Footer()
+
+
+class App(App):
+    BINDINGS = [("q", "quit", "Quit")]
+    CSS_PATH = "console.css"
+
+    SCREENS = {
+        "main": NomadMonitor,
+        "filter": Filter,
+    }
+
+    def on_mount(self) -> None:
+        self.cluster: NomadCluster = NomadCluster.from_environ()
+        self.push_screen("main")
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--debug', action='store_true', default=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", default=False)
+    parser.add_argument("--job-type")
 
-  logging.basicConfig()
+    # logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig()
 
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  if args.debug:
-    logger.setLevel(logging.DEBUG)
-    # async def on_request_start(session, context, params):
-    #   logging.getLogger('aiohttp.client').debug(f'Starting request <{params}>')
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
 
-    # async def on_request_end(session, context, params):
-    #     logging.getLogger('aiohttp.client').debug(f'Starting request <{params}>')
-
-    # logging.root.setLevel(logging.DEBUG)
-    # aiohttp_trace = aiohttp.TraceConfig()
-    # aiohttp_trace.on_request_start.append(on_request_start)
-    # aiohttp_trace.on_request_end.append(on_request_end)
-
-  logger.debug("debug level test message")
-  app = NomadMonitor()
-  app.run()
-
+    logger.debug("debug level test message")
+    app = App()
+    app.run()
